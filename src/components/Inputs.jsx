b05f6fbefd473a97bf83eb0b962ea1e22f5c83d8
Natan <developernatanmen@gmail.com>
2024-11-10 03:30:05 -0700

    modifying search functionality

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -4,9 +4,11 @@ import { BiSearch, BiCurrentLocation } from "react-icons/bi";
 
 const Inputs = ({ setQuery, setUnits }) => {
   const [city, setCity] = useState("");
+
   const handleSearchClick = () => {
     if (city !== "") setQuery({ q: city });
   };
+
   const handleLocationClick = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -15,12 +17,20 @@ const Inputs = ({ setQuery, setUnits }) => {
       });
     }
   };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearchClick();
+    }
+  };
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 ml-16 items-center justify-center space-x-4">
         <input
           value={city}
           onChange={(e) => setCity(e.currentTarget.value)}
+          onKeyDown={handleKeyDown} // Add the onKeyDown event
           type="text"
           placeholder="search by city..."
           className="text-gray-500 text-xl font-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
